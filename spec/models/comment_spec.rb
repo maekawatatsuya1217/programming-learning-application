3bb021cad0894b1cda8168f9c2254c246860d0b0
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-06-24 19:44:00 +0900

    コメント機能のテストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  describe 'コメント投稿' do
+    context 'コメント投稿ができる時' do
+     it '正しい情報が入力された時は投稿できる' do
+       expect(@comment).to be_valid
+     end
+    end
+    context 'コメント投稿できない時' do
+      it 'textが空だと投稿できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
