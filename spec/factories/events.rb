c049f5db3ac135f99556aa1cef92af4080470611
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-06-26 15:34:09 +0900

    カレンダー機能のテストコード

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :event do
-    
+    title   {'タイトル'}
+    content   {'内容'}
+    start_time { Time.now }
+    id { 1 }
+    created_at { Time.now }
+    updated_at { Time.now }
+
+    association :user
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = FactoryBot.build(:event)
+  end
+  describe 'イベント追加' do
+    context 'イベント追加ができる時' do
+     it '正しい情報が入力された時は追加できる' do
+       expect(@event).to be_valid
+     end
+    end
+    context 'イベント追加がうまくいかないとき' do
+      it 'titleが空では追加できない' do
+        @event.title = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'contentが空では追加できない' do
+        @event.content = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'start_timeが空では追加できない' do
+        @event.start_time = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Start time can't be blank")
+      end
+    end
+  end
 end
