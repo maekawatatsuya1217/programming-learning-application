dadb3a2465742fa003cd70c623e956818c43196d
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-06-29 15:45:11 +0900

    細かな修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
 
     def create
         @comment = Comment.new(comment_params)
-        if @comment.save
+        if @comment.valid?
+            @comment.save
             redirect_to "/blogs/#{@comment.blog.id}"
         else
             @blog = @comment.blog
@@ -18,5 +19,5 @@ class CommentsController < ApplicationController
     def comment_params
         params.require(:comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:blog_id])
     end
-
+    
 end
