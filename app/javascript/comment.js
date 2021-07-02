a950bd27324a55b027bd3c229f383f3d456b3833
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-02 12:41:25 +0900

    送信ボタンとフォームのidを取得

---
+++ app/javascript/comment.js
@@ -0,0 +1,8 @@
+function post (){
+    const submit = document.getElementById("submit");
+    submit.addEventListener("click", () => {
+        const form = document.getElementById("form");
+    });
+   };
+   
+   window.addEventListener('load', post);
