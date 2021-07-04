49fa6970d205bf8db4cf4b9cf3ea58f0d7912118
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-04 16:48:10 +0900

    コメント機能の非同期化

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,8 +1,35 @@
+const buildHTML = (XHR) => {
+  const comment = XHR.response.comment;
+  const user = XHR.response.user;
+  const html = `
+        <a href="/users/${comment.user_id}">${user.name}</a>
+        ${comment.text}`;
+  return html;
+};
+
 function post (){
-    const submit = document.getElementById("submit");
-    submit.addEventListener("click", () => {
-        const form = document.getElementById("form");
-    });
-   };
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const blog = document.getElementById("blog");
+    const blogId = blog.getAttribute("value");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", `/blogs/${blogId}/comments`, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;
+        };
+        const list = document.getElementById("list");
+        const formText = document.getElementById("comment_text");
+        list.insertAdjacentHTML("afterend", buildHTML(XHR));
+    formText.value = "";
+    };
+  });
+};
    
    window.addEventListener('load', post);
