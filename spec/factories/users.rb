a837b0475688c5010a7a73ca5bd0479ac0aa0aa5
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-14 15:48:00 +0900

    テストコードを再修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       name                  {'テスト'}
       email                 {Faker::Internet.free_email}
-      password              {Faker::Internet.password(min_length: 6)}
+      password              {'1a' + Faker::Internet.password(min_length: 6)}
       profile               {'オリジナルアプリ作成中です。'}
     end
   end
