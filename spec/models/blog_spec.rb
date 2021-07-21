f03945baf86cdde5bacf2db2d1868c5f090073f9
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-21 15:38:25 +0900

    テストコードを実装

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -27,6 +27,11 @@ RSpec.describe Blog, type: :model do
         @blog.valid?
         expect(@blog.errors.full_messages).to include('記事を入力してください')
       end
+      it 'categoryが空だと投稿できない' do
+        @blog.category_id = 1
+        @blog.valid?
+        expect(@blog.errors.full_messages).to include('カテゴリーを選択してください')
+      end
       it 'imageが空だと投稿できない' do
         @blog.image = nil
         @blog.valid?
